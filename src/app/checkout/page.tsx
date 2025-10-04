18f62616428b91ef408373ded02c9d45454e770d
javeri-a <javeriaqureshi746@gmail.com>
2025-10-04 21:04:49 +0500

    solved

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -582,12 +582,10 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion,  } from "framer-motion";
 import {
   CreditCard,
-  Lock,
   Shield,
-  Truck,
   User,
   ArrowLeft,
   CheckCircle,
